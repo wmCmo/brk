5ad7602a042ab9ef95622fc138b063798578a134
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2025-08-09 02:19:21 +0700

    fix: Check for `window` before accessing `localStorage` in /reset - Ensure the component is load first by using useEffect

--- src/app/menu/reset/page.tsx
+++ src/app/menu/reset/page.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function Reset() {
-
-    localStorage.setItem('client', '{}');
+    useEffect(() => { if (typeof window !== undefined) localStorage.setItem('client', '{}'); }, []);
 
     return (
         <div>Reset successfully</div>
